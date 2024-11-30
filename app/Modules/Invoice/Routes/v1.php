030d39562d2d556d415743e1a09f1cd67f0d7f89
Hosein Danamehr <hdanamehr@gmail.com>
2024-11-30 19:53:49 +0330

    fix: correct invoices route naming convention

--- app/Modules/Invoice/Routes/v1.php
+++ app/Modules/Invoice/Routes/v1.php
@@ -4,7 +4,7 @@ use App\Modules\Invoice\Http\V1\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api', 'auth:sanctum'])->prefix('v1/invoices')->name('v1.invoices.')->group(function () {
-    Route::get('invoices', [InvoiceController::class, 'index'])->name('invoices.index');
+    Route::get('', [InvoiceController::class, 'index'])->name('invoices.index');
 });
 
 
